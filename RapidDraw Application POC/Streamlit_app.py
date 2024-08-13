0eee36eb79ad5e101550e78b0b2991d267fa4536
KUNAL KALKOTWAR <94690991+Kkalkotwar@users.noreply.github.com>
2024-08-14 01:10:37 +0530

    Update Streamlit_app.py

--- RapidDraw Application POC/Streamlit_app.py
+++ RapidDraw Application POC/Streamlit_app.py
@@ -19,15 +19,15 @@ import boto3
 import os
 import time
 import pandas as pd
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+#load_dotenv()
 
-# Fetch AWS credentials and region
-aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-region_name = os.getenv("AWS_DEFAULT_REGION")
+# Fetch credentials and region
+aws_access_key_id = st.secrets("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = st.secrets("AWS_SECRET_ACCESS_KEY")
+region_name = st.secrets("AWS_DEFAULT_REGION")
 
 # Initialize the S3 and Textract clients
 s3 = boto3.client(
